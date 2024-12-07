011c4090ea1d9e4225b2a4b9d661e6f1998fa447
Sarah Vasquez <savasquez@ucdavis.edu>
2024-12-07 15:01:25 -0800

    day2

---
+++ day2/day2.py
@@ -0,0 +1,8 @@
+
+with open('input2.txt', 'r') as file: 
+        for line in file:
+            l = line.strip()
+            lists = l.split(" ")
+            for num in lists:
+                if
+            
